166a148e72d76d54618f0095fd0d14bfe3e7c555
TaumuLu <972409545@qq.com>
2024-11-20 18:37:09 +0800

    fix: eslint common config

--- eslint.config.js
+++ eslint.config.js
@@ -12,9 +12,33 @@ export default tseslint.config(
   {
     plugins: {
       prettier,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       'prettier/prettier': 'error',
+
+      // simple-import-sort
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // Packages.
+            // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+            ['^@?\\w'],
+            // Absolute imports and other imports such as Vue-style `@/foo`.
+            // Anything not matched in another group.
+            ['^'],
+            // Relative imports.
+            // Anything that starts with a dot.
+            ['^\\.'],
+            // 这里是把 import './*.scss' 自动排序到最后。其它排序规则顺序都保持和依赖包里 defaultGroups 一样
+            // 解决父组件样式覆盖不了子组件样式问题。如果 import './*.scss' 排在第一，子组件样式优先级会高于父组件
+            // Side effect imports.
+            ['^\\u0000'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
 
       // typescript-eslint
       '@typescript-eslint/no-unused-vars': 'warn',
@@ -42,34 +66,10 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-
-      // simple-import-sort
-      'simple-import-sort/imports': [
-        'error',
-        {
-          groups: [
-            // Packages.
-            // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
-            ['^@?\\w'],
-            // Absolute imports and other imports such as Vue-style `@/foo`.
-            // Anything not matched in another group.
-            ['^'],
-            // Relative imports.
-            // Anything that starts with a dot.
-            ['^\\.'],
-            // 这里是把 import './*.scss' 自动排序到最后。其它排序规则顺序都保持和依赖包里 defaultGroups 一样
-            // 解决父组件样式覆盖不了子组件样式问题。如果 import './*.scss' 排在第一，子组件样式优先级会高于父组件
-            // Side effect imports.
-            ['^\\u0000'],
-          ],
-        },
-      ],
-      'simple-import-sort/exports': 'error',
     },
   },
 )
